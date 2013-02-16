dea7cf4ca6d9fe5d4c6794b0bae499c98203ff85
Yoji SHIDARA <dara@shidara.net>
2013-02-16 14:20:11 +0900

    Cut off pictures lesser than INITIAL_RATE

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,7 @@
 class PhotosController < ApplicationController
   def index
-    @ranking = Judge.ranking
+    @ranking = Judge.ranking.select {|entry|
+      entry[:rate] > Judge::INITIAL_RATE
+    }
   end
 end
