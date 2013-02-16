a6fccc267acbf41bef9e81eda88a40e7db8a95a5
Yoji SHIDARA <dara@shidara.net>
2013-02-16 10:14:20 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ramenmash::Application.config.secret_token = 'a821f1e3009f286143c6c58ba8aab8089414c7de47516ca8178f2f1df790c5a9e19676951af4b763538347f64a3251cf53a8815c7d4129abea05b07220fa7fc7'
